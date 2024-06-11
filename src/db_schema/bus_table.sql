220279daeed6bb5db1294f4dc925d0a5a084cfe7
harry@DESKTOP-V6L <l.bangsg@gmail.com>
2024-06-11 11:04:04 +0900

    SQL schema: Change table names, and VARCHAR character count

--- src/db_schema/bus_table.sql
+++ src/db_schema/bus_table.sql
@@ -1,16 +1,16 @@
-CREATE TABLE bus_history (
+CREATE TABLE bus_initial_entry (
     initiation_time TIMESTAMPTZ NOT NULL,  -- Start time of the bus route
-    plate_number VARCHAR(20) NOT NULL,
-	route_id VARCHAR(20) NOT NULL,
+    plate_number VARCHAR(15) NOT NULL,
+	route_id VARCHAR(15) NOT NULL,
     active BOOLEAN NOT NULL,
     PRIMARY KEY (initiation_time, plate_number)
 );
 
-CREATE TABLE bus_stops (
-    plate_number VARCHAR(20) NOT NULL,
+CREATE TABLE bus_stop_record (
+    plate_number VARCHAR(15) NOT NULL,
     station_sequence INT NOT NULL,
     arrival_time TIMESTAMPTZ NOT NULL,
-    station_id VARCHAR(100),
+    station_id VARCHAR(15),
     initiation_time TIMESTAMPTZ NOT NULL,
     PRIMARY KEY (initiation_time, plate_number, station_sequence),
     FOREIGN KEY (initiation_time, plate_number) REFERENCES bus_history(initiation_time, plate_number)
--- src/db_schema/table_schema.py
+++ src/db_schema/table_schema.py
@@ -1,17 +1,17 @@
 bus_parent = """
     initiation_time TIMESTAMPTZ NOT NULL,  -- Start time of the bus route
-    plate_number VARCHAR(20) NOT NULL,
-	route_id VARCHAR(20) NOT NULL,
+    plate_number VARCHAR(15) NOT NULL,
+	route_id VARCHAR(15) NOT NULL,
     active BOOLEAN NOT NULL,
     PRIMARY KEY (initiation_time, plate_number)
 """
 
 bus_child = """
-    plate_number VARCHAR(20) NOT NULL,
+    plate_number VARCHAR(15) NOT NULL,
     station_sequence INT NOT NULL,
     arrival_time TIMESTAMPTZ NOT NULL,
-    station_id VARCHAR(100),
+    station_id VARCHAR(15),
     initiation_time TIMESTAMPTZ NOT NULL,
     PRIMARY KEY (initiation_time, plate_number, station_sequence),
-    FOREIGN KEY (initiation_time, plate_number) REFERENCES test_2_bus_initial_entry(initiation_time, plate_number)
+    FOREIGN KEY (initiation_time, plate_number) REFERENCES bus_initial_entry(initiation_time, plate_number)
 """
